d2b2c6e8f1f413f07637996b4e22d13af3ed8fed
Jakob Bichler <jbichler@login02.delftblue.tudelft.nl>
2025-02-26 13:03:25 +0100

    Fix typo in launch script

--- code_for_delft_blue/launch_dataset_generation.sh
+++ code_for_delft_blue/launch_dataset_generation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name="MRTA_dataset_generation"
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
 #SBATCH --ntasks=48
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=compute
@@ -12,5 +12,5 @@ module load 2023r1
 module load miniconda3
 
 conda activate /scratch/jbichler/generate_mrta_datasets/dataset_generation_env
-srun python /scratch/jbichler/generate_mrta_datasets/code_for_delft_blue/generate_dataset.py 
+srun python /scratch/jbichler/thesis/code_for_delft_blue/generate_dataset.py 
 conda deactivate
