46b568b9c095f63da4ddfb11e8aca725d89fb979
Jakob Bichler <jakbich@gmail.com>
2025-04-01 12:36:46 +0200

    Clean up GymRLEnv to now have discrete action space and step function gets InstantaneousSchedule object

--- reinforcement_learning/gym_environment_rl.py
+++ reinforcement_learning/gym_environment_rl.py
@@ -7,6 +7,7 @@ import torch
 sys.path.append("..")
 from rl_simulator import RL_Simulation
 
+from baselines.aswale_23.greedy_solver import greedy_scheduling
 from data_generation.problem_generator import generate_random_data_with_precedence
 from helper_functions.schedules import Instantaneous_Schedule
 from schedulers.bipartite_matching import (
@@ -22,13 +23,7 @@ class SchedulingRLEnvironment(gym.Env):
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        if seed:
-            np.random.seed(seed)
-            torch.manual_seed(seed)
-            print(f"Seed: {seed}")
-
-        self.N_THREADS = 6
-
+        self.N_THREADS = 4
         self.n_robots = 3
         self.n_tasks = 8
         self.n_skills = 3
@@ -36,15 +31,11 @@ class SchedulingRLEnvironment(gym.Env):
         self.num_robots_available_in_previous_timestep = -1
         dim_robots = 7  # (x,y,duration,[skill0, skill1, skill2], available)
         dim_tasks = 9  # (x,y,duration,[skill0, skill1, skill2],ready, assigned, incomplete)
-        self.action_space = gym.spaces.Box(
-            low=0.0,
-            high=10.0,
-            shape=(
-                self.n_robots,
-                self.n_tasks + 1,
-            ),
-            dtype=np.float32,
+
+        self.action_space = gym.spaces.Discrete(
+            self.n_tasks + 1,
         )
+
         self.observation_space = gym.spaces.Dict(
             {
                 "robot_features": gym.spaces.Box(
@@ -53,31 +44,35 @@ class SchedulingRLEnvironment(gym.Env):
                 "task_features": gym.spaces.Box(
                     low=0.0, high=1.0, shape=(self.n_tasks, dim_tasks), dtype=np.float32
                 ),
+                "task_adjacency": gym.spaces.Box(
+                    low=0.0, high=1.0, shape=(self.n_tasks, self.n_tasks), dtype=np.float32
+                ),
             }
         )
 
-        self.problem_instance = generate_random_data_with_precedence(
-            self.n_tasks, self.n_robots, self.n_skills, self.n_precedence
-        )
-
-        self.sim = RL_Simulation(
-            problem_instance=self.problem_instance,
-            debug=False,
-            move_while_waiting=True,
-        )
+        if seed:
+            np.random.seed(seed)
+            torch.manual_seed(seed)
+            print(f"Seed: {seed}")
 
     def reset(self, seed=None, options=None):
         self.problem_instance = generate_random_data_with_precedence(
             self.n_tasks, self.n_robots, self.n_skills, self.n_precedence
         )
 
+        self.worst_case_makespan = np.sum(self.problem_instance["T_e"]) + np.sum(
+            [np.max(self.problem_instance["T_t"][task]) for task in range(self.n_tasks + 1)]
+        )
+
         self.sim = RL_Simulation(
             problem_instance=self.problem_instance,
             debug=False,
             move_while_waiting=True,
         )
 
-        return self._get_observation()
+        self.greedy_makespan = greedy_scheduling(self.problem_instance).makespan
+
+        return self._get_observation(), {}
 
     def _get_observation(self):
         robot_features, task_features = self.sim.return_task_robot_states()
@@ -85,52 +80,29 @@ class SchedulingRLEnvironment(gym.Env):
         return {
             "robot_features": robot_features,
             "task_features": task_features,
+            "task_adjacency": self.sim.task_adjacency,
         }
 
     def calculate_reward(self):
         if self.sim.sim_done:
-            return -self.sim.makespan
+            print(
+                "Simulation done with makespan:",
+                self.sim.makespan,
+                "and greedy makespan:",
+                self.greedy_makespan,
+            )
+            return -(self.sim.makespan - self.greedy_makespan)
+
         else:
             return 0.0
 
-    def step(self, action):
-        robot_assignments = {}
-        available_robots = [robot for robot in self.sim.robots if robot.available]
-        incomplete_tasks = [
-            task for task in self.sim.tasks if task.incomplete and task.status == "PENDING"
-        ]
-
-        # Check if all normal tasks are done -> send all robots to the exit task
-        if len(incomplete_tasks) == 1:  # Only end task incomplete
-            for robot in available_robots:
-                robot_assignments[robot.robot_id] = incomplete_tasks[0].task_id
-                robot.current_task = incomplete_tasks[0]
-        else:  # Normal assignment
-            action = torch.clamp(action, min=1e-6)
-            reward_start_end = torch.ones(self.n_robots, 1).to(self.device) * (-1000)
-            action = torch.cat((reward_start_end, action, reward_start_end), dim=1)
-
-            bipartite_matching_solution = solve_bipartite_matching(
-                action, self.sim, n_threads=self.N_THREADS
-            )
-            filtered_solution = filter_redundant_assignments(bipartite_matching_solution, self.sim)
-            filtered_solution = filter_overassignments(filtered_solution, self.sim)
-
-            robot_assignments = {
-                robot: task for (robot, task), val in filtered_solution.items() if val == 1
-            }
-
-            self.sim.assign_tasks_to_robots(
-                Instantaneous_Schedule(robot_assignments), self.sim.robots
-            )
-
-            self.sim.find_highest_non_idle_reward(action)
+    def step(self, action: Instantaneous_Schedule):
+        self.sim.assign_tasks_to_robots(action, self.sim.robots)
+        truncated = False
 
         while not self.sim.sim_done:
             available = [r for r in self.sim.robots if r.available]
             current_available = len(available)
-            # print(f"Current available robots: {current_available}")
-            # print(f"Previous available robots: {self.num_robots_available_in_previous_timestep}")
             self.sim.step()
             # Roll out the simulation until we reach the next decision step (new available robots)
             if self.sim.sim_done or (
@@ -141,9 +113,15 @@ class SchedulingRLEnvironment(gym.Env):
                 break
 
             self.num_robots_available_in_previous_timestep = current_available
+            # Force termination if timestep exceeds worst-case threshold
+            if self.sim.timestep >= self.worst_case_makespan:
+                self.sim.finish_simulation()
+                self.sim.makespan = self.worst_case_makespan
+                print(f"Scheduler did not find a feasible solution at timestep {self.sim.timestep}")
+                truncated = True
+                break
 
         reward = self.calculate_reward()
+        terminated = self.sim.sim_done
 
-        done = self.sim.sim_done
-
-        return self._get_observation(), reward, done, {}
+        return self._get_observation(), reward, terminated, truncated, {}
