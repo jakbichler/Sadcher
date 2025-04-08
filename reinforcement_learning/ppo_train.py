ecdd7955b0c070f82a7254f115cde6a09ec306ba
Jakob Bichler <jakbich@gmail.com>
2025-04-08 12:13:28 +0200

    Add argparse to ppo train script

--- reinforcement_learning/ppo_train.py
+++ reinforcement_learning/ppo_train.py
@@ -1,3 +1,5 @@
+import argparse
+
 import gymnasium as gym
 import torch
 from policy_value import SchedulerPolicy, SchedulerValue
@@ -7,28 +9,55 @@ from skrl.memories.torch import RandomMemory
 from skrl.trainers.torch.parallel import PARALLEL_TRAINER_DEFAULT_CONFIG, ParallelTrainer
 
 if __name__ == "__main__":
-    N_ENVS = 64
+    argument_parser = argparse.ArgumentParser(
+        description="Train the model using PPO implemenation of SK RL"
+    )
+
+    argument_parser.add_argument(
+        "--N_ENVS",
+        type=int,
+        default=32,
+        help="Number of parallel environments",
+    )
+
+    argument_parser.add_argument(
+        "--N_ROLLOUTS",
+        type=int,
+        default=256,
+        help="Number of rollouts enviroment steps before PPO update",
+    )
+
+    argument_parser.add_argument(
+        "--pretrained",
+        action="store_true",
+        default=False,
+        help="Use pretrained model",
+    )
+
+    args = argument_parser.parse_args()
 
     env_id = "SchedulingRLEnvironment-v0"
     gym.register(id=env_id, entry_point="gym_environment_rl:SchedulingRLEnvironment")
-    envs = gym.make_vec(env_id, num_envs=N_ENVS, vectorization_mode="async")
+    envs = gym.make_vec(env_id, num_envs=args.N_ENVS, vectorization_mode="async")
     envs = wrap_env(envs)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    memory = RandomMemory(memory_size=128, num_envs=N_ENVS, device=device)
-
-    policy_model = SchedulerPolicy(envs.observation_space, envs.action_space, device)
-    value_model = SchedulerValue(envs.observation_space, envs.action_space)
-    models = {"policy": policy_model, "value": value_model}
-
     ppo_config = PPO_DEFAULT_CONFIG.copy()
-    ppo_config["rollouts"] = 256
+    ppo_config["rollouts"] = args.N_ROLLOUTS
     ppo_config["learning_epochs"] = 4
     ppo_config["mini_batches"] = 4
     ppo_config["discount_factor"] = 0.99
     ppo_config["learning_rate"] = 3e-4
-    ppo_config["experiment"]["write_interval"] = 1000
+    ppo_config["experiment"]["write_interval"] = 100
+
+    memory = RandomMemory(memory_size=args.N_ROLLOUTS, num_envs=args.N_ENVS, device=device)
+
+    policy_model = SchedulerPolicy(
+        envs.observation_space, envs.action_space, device, pretrained=args.pretrained
+    )
+    value_model = SchedulerValue(envs.observation_space, envs.action_space)
+    models = {"policy": policy_model, "value": value_model}
 
     agent = PPO(
         models=models,
@@ -40,7 +69,8 @@ if __name__ == "__main__":
     )
     agent.init()
 
-    cfg = PARALLEL_TRAINER_DEFAULT_CONFIG.copy()
-    cfg["headless"] = True
-    trainer = ParallelTrainer(cfg=cfg, env=envs, agents=[agent])
+    trainer_cfg = PARALLEL_TRAINER_DEFAULT_CONFIG.copy()
+    trainer_cfg["timesteps"] = 1_000_000
+    trainer_cfg["headless"] = True
+    trainer = ParallelTrainer(cfg=trainer_cfg, env=envs, agents=[agent])
     trainer.train()
