c8fca63926c380ec48d82b89830161dbce630452
Jakob Bichler <jakbich@gmail.com>
2025-04-23 14:22:57 +0200

    Adjsut PPO train script to select ZeroCritic, and usage with Continuous env + flags

--- reinforcement_learning/ppo_train.py
+++ reinforcement_learning/ppo_train.py
@@ -11,6 +11,7 @@ from skrl.envs.torch import wrap_env
 from skrl.memories.torch import RandomMemory
 from skrl.trainers.torch.parallel import PARALLEL_TRAINER_DEFAULT_CONFIG, ParallelTrainer
 
+from models.policy_value_continuous import ContinuousSchedulerPolicy, ZeroCritic
 from models.policy_value_discrete import SchedulerPolicy, SchedulerValue
 
 
@@ -88,19 +89,46 @@ if __name__ == "__main__":
         help="Use pretrained model",
     )
 
+    argument_parser.add_argument(
+        "--continuous_RL",
+        action="store_true",
+        default=False,
+        help="Use continuous RL",
+    )
+
+    argument_parser.add_argument(
+        "--zero_critic",
+        action="store_true",
+        default=False,
+        help="Use zero critic instead of a learned one",
+    )
+
     args = argument_parser.parse_args()
     use_idle = not args.not_use_idle
 
-    env_id = "SchedulingRLEnvironment-v0"
-    gym.register(
-        id=env_id,
-        entry_point="gym_environment_rl:SchedulingRLEnvironment",
-        kwargs={
-            "problem_type": args.problem_type,
-            "use_idle": use_idle,
-            "subtractive_assignment": True,
-        },
-    )
+    if args.continuous_RL:
+        env_id = "SchedulingRLEnvironmentContinuous-v0"
+
+        gym.register(
+            id=env_id,
+            entry_point="continuous_gym_environment_rl:ContinuousSchedulingRLEnvironment",
+            kwargs={
+                "problem_type": args.problem_type,
+                "use_idle": use_idle,
+                "subtractive_assignment": False,
+            },
+        )
+    else:
+        env_id = "SchedulingRLEnvironmentDiscrete-v0"
+        gym.register(
+            id=env_id,
+            entry_point="discrete_gym_environment_rl:SchedulingRLEnvironment",
+            kwargs={
+                "problem_type": args.problem_type,
+                "use_idle": use_idle,
+                "subtractive_assignment": True,
+            },
+        )
     envs = gym.make_vec(
         env_id,
         num_envs=args.N_ENVS,
@@ -148,31 +176,64 @@ if __name__ == "__main__":
 
     ppo_config = PPO_DEFAULT_CONFIG.copy()
     ppo_config["rollouts"] = args.N_ROLLOUTS
-    ppo_config["learning_epochs"] = 10
-    ppo_config["mini_batches"] = 4
+    ppo_config["learning_epochs"] = 6
+    ppo_config["mini_batches"] = 16
     ppo_config["discount_factor"] = 0.99
-    ppo_config["learning_rate"] = 3e-4
+    ppo_config["learning_rate"] = 2e-4
     ppo_config["mixed_precision"] = False
     ppo_config["experiment"]["write_interval"] = args.N_ROLLOUTS
-    ppo_config["experiment"]["checkpoint_interval"] = 10_000
-    ppo_config["kl_threshold"] = 0.02
+    ppo_config["experiment"]["checkpoint_interval"] = 2_000
+    ppo_config["kl_threshold"] = 0.03
     ppo_config["clip_ratio"] = 0.1
-    ppo_config["entropy_loss_scale"] = 0.02
+    ppo_config["entropy_loss_scale"] = 0.001
+    if args.zero_critic:
+        ppo_config["value_loss_scale"] = 0.0
+    else:
+        ppo_config["value_loss_scale"] = 1.0
+
+    log_stddev_init = 0
 
     memory = RandomMemory(memory_size=args.N_ROLLOUTS, num_envs=args.N_ENVS, device=device)
 
-    policy_model = SchedulerPolicy(
-        envs.observation_space,
-        envs.action_space,
-        device,
-        policy_config=policy_config,
-        pretrained=args.IL_pretrained_policy,
-        use_idle=use_idle,
-        use_positional_encoding=False,
-        debug=debug,
-    )
+    if args.continuous_RL:
+        policy_model = ContinuousSchedulerPolicy(
+            envs.observation_space,
+            envs.action_space,
+            device,
+            policy_config=policy_config,
+            pretrained=args.IL_pretrained_policy,
+            use_idle=use_idle,
+            debug=debug,
+            clip_log_std=True,
+            min_log_std=-20,
+            max_log_std=2,
+            log_stddev_init=log_stddev_init,
+        )
 
-    value_model = SchedulerValue(envs.observation_space, envs.action_space, value_config)
+    else:
+        policy_model = SchedulerPolicy(
+            envs.observation_space,
+            envs.action_space,
+            device,
+            policy_config=policy_config,
+            pretrained=args.IL_pretrained_policy,
+            use_idle=use_idle,
+            use_positional_encoding=False,
+            debug=debug,
+        )
+
+    print(type(policy_model))
+
+    if args.zero_critic:
+        value_model = ZeroCritic(
+            envs.observation_space,
+            envs.action_space,
+            device,
+        )
+    else:
+        value_model = SchedulerValue(envs.observation_space, envs.action_space, value_config).to(
+            device
+        )
 
     agent = PPO(
         models={"policy": policy_model, "value": value_model},
