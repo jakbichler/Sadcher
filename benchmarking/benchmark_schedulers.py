55bf135b30a83bf9f63e16b801be8ea716f627e4
Jakob Bichler <jakbich@gmail.com>
2025-05-05 15:12:54 +0200

    Adjust benchmark script to work with stochastic scheduler and keep best found runs

--- benchmarking/benchmark_schedulers.py
+++ benchmarking/benchmark_schedulers.py
@@ -21,21 +21,60 @@ from visualizations.benchmark_visualizations import (
     print_final_results,
 )
 
+
+def run_one_simulation(problem_instance, scheduler_name, checkpoint_path):
+    worst_case_makespan = np.sum(problem_instance["T_e"]) + np.sum(
+        [np.max(problem_instance["T_t"][task]) for task in range(len(problem_instance["T_e"]))]
+    )
+
+    sim = Simulation(problem_instance, scheduler_name)
+    scheduler = create_scheduler(
+        scheduler_name,
+        checkpoint_path,
+        model_name="8t3r3s",
+        duration_normalization=sim.duration_normalization,
+        location_normalization=sim.location_normalization,
+        stddev=0.5,
+    )
+    feasible = True
+    current_run_computation_times = []
+    while not sim.sim_done:
+        start_time = time.time()
+        if isinstance(scheduler, SadcherScheduler):
+            predicted_reward, instantaneous_schedule = scheduler.calculate_robot_assignment(sim)
+            sim.find_highest_non_idle_reward(predicted_reward)
+        else:
+            instantaneous_schedule = scheduler.calculate_robot_assignment(sim)
+        current_run_computation_times.append(time.time() - start_time)
+
+        sim.assign_tasks_to_robots(instantaneous_schedule)
+        sim.step_until_next_decision_point()
+
+        if sim.timestep > worst_case_makespan:
+            sim.makespan = worst_case_makespan  # No feasible solution found
+            print(f"Scheduler {scheduler_name} did not find a feasible solution.")
+            feasible = False
+            break
+
+    return sim.makespan, feasible, current_run_computation_times
+
+
 if __name__ == "__main__":
-    n_tasks = 30
-    n_robots = 10
+    n_tasks = 8
+    n_robots = 3
     n_skills = 3
     n_precedence = 3
-    seed = 12312
+    n_stochastic_runs = 20
+    seed = 2
     np.random.seed(seed)
     model_name = "8t3r3s"
     checkpoint_path = (
-        "/home/jakob/thesis/imitation_learning/checkpoints/hyperparam_2_8t3r3s/best_checkpoint.pt"
+        "/home/jakob/thesis/imitation_learning/checkpoints/8t3r3s_models/model_0/best_checkpoint.pt"
     )
 
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument(
-        "--including_milp",
+        "--include_milp",
         default=False,
         action="store_true",
         help="Include MILP in the comparison",
@@ -49,12 +88,22 @@ if __name__ == "__main__":
         action="store_true",
         help="Allow robots to move while waiting for tasks",
     )
+
+    arg_parser.add_argument(
+        "--include_stochastic_sadcher",
+        default=False,
+        action="store_true",
+        help="Include stochastic Sadcher in the comparison",
+    )
+
     args = arg_parser.parse_args()
 
-    if args.including_milp:
-        scheduler_names = ["milp", "greedy", "sadcher", "random_bipartite"]
-    else:
-        scheduler_names = ["greedy", "sadcher", "random_bipartite"]
+    scheduler_names = ["greedy", "sadcher", "random_bipartite"]
+
+    if args.include_milp:
+        scheduler_names.append("milp")
+    if args.include_stochastic_sadcher:
+        scheduler_names.append("stochastic_sadcher")
 
     makespans = {scheduler: [] for scheduler in scheduler_names}
     feasible_makespans = {scheduler: [] for scheduler in scheduler_names}
@@ -69,10 +118,6 @@ if __name__ == "__main__":
             n_tasks, n_robots, n_skills, n_precedence
         )
 
-        worst_case_makespan = np.sum(problem_instance["T_e"]) + np.sum(
-            [np.max(problem_instance["T_t"][task]) for task in range(n_tasks + 1)]
-        )
-
         for scheduler_name in scheduler_names:
             if scheduler_name == "milp":
                 start_time = time.time()
@@ -82,48 +127,44 @@ if __name__ == "__main__":
                 computation_times[scheduler_name].append(time.time() - start_time)
                 makespans[scheduler_name].append(optimal_schedule.makespan)
                 feasible_makespans[scheduler_name].append(optimal_schedule.makespan)
-            else:
-                # Non-MILP olutins need simulation to roll out schedule
-                sim = Simulation(problem_instance, scheduler_name)
-                scheduler = create_scheduler(
-                    scheduler_name,
-                    checkpoint_path,
-                    model_name="8t3r3s",
-                    duration_normalization=sim.duration_normalization,
-                    location_normalization=sim.location_normalization,
-                )
-                feasible = True
-                current_run_computation_times = []
-                while not sim.sim_done:
-                    start_time = time.time()
-                    if isinstance(scheduler, SadcherScheduler):
-                        predicted_reward, instantaneous_schedule = (
-                            scheduler.calculate_robot_assignment(sim)
-                        )
-                        sim.find_highest_non_idle_reward(predicted_reward)
-                    else:
-                        instantaneous_schedule = scheduler.calculate_robot_assignment(sim)
-                    current_run_computation_times.append(time.time() - start_time)
 
-                    sim.assign_tasks_to_robots(instantaneous_schedule)
-                    sim.step_until_next_decision_point()
+            elif scheduler_name == "stochastic_sadcher":
+                best_ms, ct = float("inf"), None
 
-                    if sim.timestep > worst_case_makespan:
-                        sim.makespan = worst_case_makespan  # No feasible solution found
+                for _ in tqdm(range(n_stochastic_runs)):
+                    makespan, feasible, current_run_computation_times = run_one_simulation(
+                        problem_instance, scheduler_name, checkpoint_path
+                    )
+
+                    if not feasible:
                         infeasible_count[scheduler_name] += 1
-                        print(
-                            f"Scheduler {scheduler_name} did not find a feasible solution at {iteration}"
-                        )
-                        feasible = False
-                        break
 
-                makespans[scheduler_name].append(sim.makespan)
+                    best_ms = min(best_ms, makespan) if feasible else best_ms
+
+                makespans[scheduler_name].append(best_ms)
                 average_computation_time_per_assignment = np.mean(current_run_computation_times)
                 full_computation_time = np.sum(current_run_computation_times)
                 computation_times[scheduler_name].append(average_computation_time_per_assignment)
 
                 if feasible:
-                    feasible_makespans[scheduler_name].append(sim.makespan)
+                    feasible_makespans[scheduler_name].append(best_ms)
+
+            else:
+                makespan, feasible, current_run_computation_times = run_one_simulation(
+                    problem_instance, scheduler_name, checkpoint_path
+                )
+
+                if not feasible:
+                    infeasible_count[scheduler_name] += 1
+
+                makespans[scheduler_name].append(makespan)
+                computation_times[scheduler_name].append(np.mean(current_run_computation_times))
+                average_computation_time_per_assignment = np.mean(current_run_computation_times)
+                full_computation_time = np.sum(current_run_computation_times)
+                computation_times[scheduler_name].append(average_computation_time_per_assignment)
+
+                if feasible:
+                    feasible_makespans[scheduler_name].append(makespan)
 
         iteration_results = sorted(
             (makespans[scheduler_name][-1], scheduler_name) for scheduler_name in scheduler_names
@@ -160,7 +201,7 @@ if __name__ == "__main__":
     )
 
     # MILP vs Sadcher comparison (if included)
-    if args.including_milp:
+    if args.include_milp:
         compare_makespans_1v1(
             axs[1, 1], makespans["milp"], makespans["sadcher"], "MILP", "Sadcher-RT"
         )
